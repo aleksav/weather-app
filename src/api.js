6cdd74ccef346ff73e3a22fe3ecdb6bf1244f09d
Austin Booth <austinbooth@yahoo.co.uk>
2020-10-26 19:00:19 +0000

    adds ability to change units

--- src/api.js
+++ src/api.js
@@ -5,9 +5,11 @@ const instance = axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5/",
 });
 
-const getWeatherData = (location) => {
+const getWeatherData = (location, units = "metric") => {
   return instance
-    .get(`weather?q=${location}&appid=${token.OPEN_WEATHER_TOKEN}`)
+    .get(
+      `weather?q=${location}&appid=${token.OPEN_WEATHER_TOKEN}&units=${units}`
+    )
     .then(({ data }) => data)
     .catch((err) => console.log(err));
 };
