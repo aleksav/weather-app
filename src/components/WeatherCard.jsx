01e5e7b0c4f13251b3a14ede6cd370115348b886
Austin Booth <austinbooth@yahoo.co.uk>
2020-10-27 12:22:58 +0000

    renames component and adds use of API data

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import getWeatherData from "../api";
+import Loader from "./Loader";
+
+const WeatherCard = (props) => {
+  const { location } = props;
+  const [weatherData, setWeatherData] = useState(null);
+  if (!weatherData)
+    getWeatherData(location).then((data) => setWeatherData(data));
+
+  if (!weatherData) return <Loader />;
+
+  return (
+    <div className="weather-card">
+      <h2>
+        {weatherData.name}, {weatherData.sys.country}
+      </h2>
+      <p className="location-temp">{Math.round(weatherData.main.temp)} °C</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+        alt={weatherData.weather[0].main}
+      />
+      <p className={"weather-description"}>
+        {weatherData.weather[0].description}
+      </p>
+    </div>
+  );
+};
+
+export default WeatherCard;
